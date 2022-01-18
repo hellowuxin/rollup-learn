e7b4997bfcbd848c8e2e2a35ad7c541395a19a6b
wuxin <hello_wuxin@qq.com>
2022-01-18 15:46:31 +0800

    修改配置

--- rollup.config.app.js
+++ rollup.config.app.js
@@ -1,21 +1,30 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import replace from '@rollup/plugin-replace';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
+	input: 'src/main.tsx',
 	output: {
 		file: 'public/bundle.js',
 		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
 		sourcemap: true
 	},
 	plugins: [
+    replace({
+      preventAssignment: true,
+      values: {
+        'process.env.NODE_ENV': JSON.stringify('development')
+      }
+    }),
+    typescript(),
 		resolve(), // tells Rollup how to find date-fns in node_modules
-		commonjs(), // converts date-fns to ES modules
+		commonjs(), // converts  to ES modules
 		production && terser() // minify, but only in production
 	]
 };
