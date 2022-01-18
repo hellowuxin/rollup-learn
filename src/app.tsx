3ba19e486ac9fe481369ecbd7b528952d9dd0c78
wuxin <hello_wuxin@qq.com>
2022-01-18 17:12:25 +0800

    app

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import DoveButton from './components/button'
+
+const App = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <DoveButton>确定</DoveButton>
+    </div>
+  )
+}
+
+export default App
