108ecef8cd32dd9cc3aa85c2a73ce1c7c4ac590a
wuxin <hello_wuxin@qq.com>
2022-01-18 11:47:03 +0800

    插件

--- bundle.js
+++ bundle.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// src/foo.js
-var foo = 'hello world!';
+var version = "1.0.0";
 
 // src/main.js
+
 function main () {
-  console.log(foo);
+  console.log('version ' + version);
 }
 
 module.exports = main;
