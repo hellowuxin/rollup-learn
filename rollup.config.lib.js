543a31f170a3e417603b4b90ff0dc0435bd79bd3
wuxin <hello_wuxin@qq.com>
2022-01-18 17:21:42 +0800

    修改配置

--- rollup.config.lib.js
+++ rollup.config.lib.js
@@ -1,10 +1,39 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import replace from '@rollup/plugin-replace';
+import alias from '@rollup/plugin-alias';
+import path from 'path'
+import postcss from 'rollup-plugin-postcss'
 import pkg from './package.json';
 
 export default {
-  input: 'src/main.js',
+  input: 'src/components/index.tsx',
   external: ['react'],
   output: [
     { file: pkg.main, format: 'cjs', exports: 'auto' },
     { file: pkg.module, format: 'es', exports: 'auto' }
-  ]
+  ],
+  plugins: [
+    alias({
+      entries: [
+        { find: '@', replacement: path.resolve(__dirname, 'src') },
+      ],
+    }),
+    replace({
+      preventAssignment: true,
+      values: {
+        'process.env.NODE_ENV': JSON.stringify('development')
+      }
+    }),
+		resolve(),
+		commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    postcss({
+      use: {
+        less: { javascriptEnabled: true }
+      },
+      extract: true
+    })
+	]
 }
